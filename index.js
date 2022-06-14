003ad9648cef8c058093376ec55383394fbd46fc
seemasheikh <98578905+seemasheikh@users.noreply.github.com>
2022-06-14 00:32:56 -0700

    my assignment 6 to 9

---
+++ index.js
@@ -0,0 +1,97 @@
+// var a=10;
+// alert("the value of a is"+a);
+// a=++a;
+// alert("now the value of a is"+a)
+// a=++a;
+// alert("the value of a is"+a)
+// a=--a;
+// alert("now the value of a is"+a)
+// alert("the value of a-- is"+a)
+// a=--a;
+// alert("now the value of a is"+a)
+
+// // q-no-2
+// var a=2,b=1;
+// var result=--a - --b + ++b +b--;
+// alert(result)
+// alert("a is"+a)
+// alert("b is"+b)
+// alert("the result is"+result)
+
+// var userName=prompt("enter your name?");
+// alert("wellCome home!!!"+userName);
+
+// // q-no-4
+
+// var userSchool=prompt("enter your school name?");
+
+// alert("wellCome to!!!"+userSchool);
+
+// // q-no-5
+// var num=prompt("enter a number");
+// if(num==4){
+// var table1=4;
+// console.log("4*1="+table1)
+// var table2=8;
+// console.log("4*2="+table2)
+// var table3=12;
+// console.log("4*3="+table3)
+// var table4=16;
+// console.log("4*4="+table4)
+// var table5=20;
+// console.log("4*5="+table5)
+// var table6=24;
+// console.log("4*6="+table6)
+// var table7=28;
+// console.log("4*7="+table7)
+// var table8=32;
+// console.log("4*8="+table8)
+// var table9=36;
+// console.log("4*9="+table9)
+// var table10=40;
+// console.log("4*10="+table10)
+// }else{
+//     var table1=5;
+// console.log("5*1="+table1)
+// var table2=10;
+// console.log("5*2="+table2)
+// var table3=15;
+// console.log("5*3="+table3)
+// var table4=20;
+// console.log("5*4="+table4)
+// var table5=25;
+// console.log("5*5="+table5)
+// var table6=30;
+// console.log("5*6="+table6)
+// var table7=35;
+// console.log("5*7="+table7)
+// var table8=40;
+// console.log("5*8="+table8)
+// var table9=45;
+// console.log("5*9="+table9)
+// var table10=50;
+// console.log("5*10="+table10)
+
+// }
+var sub1="english";
+var sub2="urdu";
+var sub3="maths";
+var totalMarks=100;
+console.log(totalMarks)
+console.log("english   "  +totalMarks + "   54"+"   54%")
+console.log("urdu      "  +totalMarks + "    54"+"   54%")
+console.log("math      "  +totalMarks + "    48"+"    48")
+var totalMarks=300;
+var marksObtained=156;
+var percentage=marksObtained*100/totalMarks;
+console.log("        "+totalMarks+ "       "+ marksObtained+"       "+percentage+"%")
+
+
+
+
+
+
+
+
+
+
